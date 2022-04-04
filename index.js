993887badfcb362a1c8bd68b652064d30af61c76
Johnny Chen <johnnyc1314@gmail.com>
2022-04-04 17:03:17 -0400

    commit tree 53c0cbb897b8cd4a7a003a04c3d2c64419940141

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(n){
+    return Math.abs(42 - n)
+}
+function distanceFromHqInFeet(n){
+    return distanceFromHqInBlocks(n)*264
+}
+function distanceTravelledInFeet(start,end){
+    return Math.abs((start - end) * 264)
+}
+function calculatesFarePrice(start, end){
+    let distance = distanceTravelledInFeet(start, end);
+    let fare = 0
+    if (distance > 2500){
+        return 'cannot travel that far'
+    }
+    else if (distance > 2000){
+        return 25
+    }
+    else if (distance <= 400){
+        return fare
+    }
+    else {
+        distance -= 400
+        fare += distance*.02
+        return fare
+    }
+
+
+}
